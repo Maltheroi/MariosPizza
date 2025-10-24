047fb1b39654a9d82cd2d84ff7d539b2c6376496
Maltheroi <maro0013@stud.ek.dk>
2025-10-24 11:22:12 +0200

    Menu

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
+        List<Menu> pizzaMenu = Menu.laesPizzaFraMenu();
+
+        System.out.println("=== Mario's Pizza Menu ===");
 
 
+        for (int i = 0; i < pizzaMenu.size(); i++) {
+            System.out.println((i + 1) + ". " + pizzaMenu.get(i).getNavn());
         }
+
+        System.out.println("===========================");
     }
+}
--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Menu {
+    private String navn;
+
+
+    public Menu(String navn) {
+        this.navn = navn;
+    }
+
+
+    public String getNavn() {
+        return navn;
+    }
+
+
+    public String toString() {
+        return navn;
+    }
+
+
+
+    public static List<Menu> laesPizzaFraMenu() {
+
+        String line = "";
+        String menuFil = "menu.txt";
+        List<Menu> menu = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(menuFil))) {
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty()) continue;
+
+                Menu pizza = new Menu(line);
+                menu.add(pizza);
+
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Fejl ved læsning af menu: " + e.getMessage());
+
+        }
+        return menu;
+
+    }
+
+
+
+
 }
